9498fe4c95b2670ccd33d144035ccd5738fa7848
cherryzhong@efun.com <cherryzhong@efun.com>
2017-11-30 18:08:08 +0800

    提交代码

---
+++ src/main/java/com/efun/micro/Application.java
@@ -0,0 +1,33 @@
+package com.efun.micro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * SpringBoot启动类
+ */
+@ImportResource(locations = {"classpath:applicationContext-springboot.xml"})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@ComponentScan
+@Controller
+@EnableCaching
+public class Application {
+
+    @ResponseBody
+    @RequestMapping("/")
+    public String hello() {
+        return "hello world!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
